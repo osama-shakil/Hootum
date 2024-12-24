730055fafd935b22e1589b7d555acc949215db7b
Muhammad Osama <72553062+osama-shakil@users.noreply.github.com>
2024-12-24 22:45:12 +0500

    code pushed

--- src/components/landing/marketplace/FilterSection1.jsx
+++ src/components/landing/marketplace/FilterSection1.jsx
@@ -112,7 +112,6 @@ export default function FilterSection1() {
           ))}
         </div>
       </div>
-
       {/* Filter Button - Show on desktop only */}
       <Button
         variant="outline"
